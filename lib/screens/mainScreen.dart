d4bac4d0bbec69dbed1efba577abd296a0ee387d
jason1110 <Jason.easterly11@gmail.com>
2020-12-22 08:55:35 -0700

    version before flutter clean

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import '../services/geolocator.dart';
+// import '../services/geolocator.dart';
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 class MainScreen extends StatefulWidget {
   @override
@@ -9,8 +13,78 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+  StreamSubscription _locationSubscription;
+  Location _locationTracker = Location();
+  Marker marker;
+  Circle circle;
+  GoogleMapController _controller;
   
-  
+  static final CameraPosition initialLocation = CameraPosition(
+  target: LatLng(39.75319, -105.00009),
+  zoom: 14.4746,
+  );
+
+  Future<Uint8List> getMarker() async {
+    ByteData byteData = await DefaultAssetBundle.of(context).load("images/car_icon.png");
+    return byteData.buffer.asUint8List();
+  }
+
+  void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
+    LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    this.setState(() {
+      marker = Marker(
+        markerId: MarkerId('home'),
+        position: latlng,
+        rotation: newLocalData.heading,
+        draggable: false,
+        zIndex: 2,
+        flat: true,
+        anchor: Offset(0.5, 0.5),
+        icon: BitmapDescriptor.fromBytes(imageData)
+      );
+      circle = Circle(
+        circleId: CircleId('car'),
+        radius: newLocalData.accuracy,
+        zIndex: 1,
+        strokeColor: Colors.lightGreen,
+        center: latlng,
+        fillColor: Colors.lightGreen.withAlpha(70)
+      );
+    });
+  }
+
+  void getCurrentLocation() async {
+    try {
+      Uint8List imageData = await getMarker();
+      var location = await _locationTracker.getLocation();
+
+      updateMarkerAndCircle(location, imageData);
+
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+      _locationSubscription = _locationTracker.onLocationChanged.listen((newLocalData){
+        if (_controller != null) {
+          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+            bearing: 192.8334901395799,
+            target: LatLng(newLocalData.latitude, newLocalData.longitude),
+            tilt: 0,
+            zoom: 15.00,
+            ),
+          ));
+          updateMarkerAndCircle(newLocalData, imageData);
+        }
+      });
+    } on PlatformException catch (error) {
+      if (error.code == 'PERMISSION_DENIED') {
+        debugPrint('Permission Denied');
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,21 +93,33 @@ class _MainScreenState extends State<MainScreen> {
         title: Text("Park"),
         centerTitle: true,
       ),
-      body: Center(
+      body:  Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text("Get location"),
-              onPressed: () {
-                getLocation();
-              },
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(80.0),
+              child: SizedBox(
+                height: 200.0,
+                child: GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: initialLocation,
+                  markers: Set.of((marker != null) ? [marker] : []),
+                  circles: Set.of((circle != null) ? [circle] : []),
+                  onMapCreated: (GoogleMapController controller) {
+                  _controller = controller;
+                  },
+                ),
+              ),
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () {
+          getCurrentLocation();
+        },
+      ),
     );
   }
-
 }
-
